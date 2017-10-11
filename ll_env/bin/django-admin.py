5367d23b96b2c532d94e708df4022a1af2253882
Kevin Dwan <Kevin@Kevins-MacBook-Air.local>
2017-10-10 18:56:27 -0700

    My first commit

---
+++ ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Kevin/Documents/Coding_Practice/projects/learninglog/ll_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
